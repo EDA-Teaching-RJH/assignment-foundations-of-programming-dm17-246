273213caa08ed625bd2eb4f255ee353799119f8f
dm17-246 <dm17@kent.ac.uk>
2026-02-15 21:15:05 +0000

    Feature 1, 2.0

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,5 +1,8 @@
-crew_names = ["Picard", "Riker", "Kirk", "Spock", "Uhura"]
-crew_ranks = ["Commodore", "Captain", "Commander", "Lieutenant","Ensign"]
-crew_divisions = ["Command", "Operations", "Science", "Medical", "Engineering"]
-crew_ids = ["NC1001", "NC1002", "NC1003", "NC1004", "NC1005"]
+def init_crew():
+    crew_names = ["Picard", "Riker", "Kirk", "Spock", "Uhura"]
+    crew_ranks = ["Commodore", "Captain", "Commander", "Lieutenant","Ensign"]
+    crew_divisions = ["Command", "Operations", "Science", "Medical", "Engineering"]
+    crew_ids = ["NC1001", "NC1002", "NC1003", "NC1004", "NC1005"]
+    
+    return crew_names, crew_ranks, crew_divisions, crew_ids
 
