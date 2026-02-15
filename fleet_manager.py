870558f0cb69fb53c514a48df8cfa7d274360846
dm17-246 <dm17@kent.ac.uk>
2026-02-15 21:13:09 +0000

    Feature 1

---
+++ fleet_manager.py
@@ -0,0 +1,5 @@
+crew_names = ["Picard", "Riker", "Kirk", "Spock", "Uhura"]
+crew_ranks = ["Commodore", "Captain", "Commander", "Lieutenant","Ensign"]
+crew_divisions = ["Command", "Operations", "Science", "Medical", "Engineering"]
+crew_ids = ["NC1001", "NC1002", "NC1003", "NC1004", "NC1005"]
+
